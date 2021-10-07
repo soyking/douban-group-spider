dba6a76ea494944edf7355ed220873d17bed4617
soyking <soyking@outlook.com>
2021-10-07 13:16:28 +0000

    fix(group): fix http client return 418

--- group/group.go
+++ group/group.go
@@ -2,37 +2,50 @@ package group
 
 import (
 	"errors"
-	"github.com/PuerkitoBio/goquery"
+	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 var (
 	ErrorForbidden = errors.New("request is forbidden, maybe too frequently")
 )
 
-func getGroupURL(name string, start ...int) string {
+func groupURL(name string, start ...int) string {
 	s := "0"
 	if len(start) > 0 {
 		s = strconv.Itoa(start[0])
 	}
-	return "https://www.douban.com/group/" + name + "/discussion?start=" + s
+	return host + "/group/" + name + "/discussion?start=" + s
 }
 
-// 获取豆瓣小组的内容 start 表示从第几条开始 默认 0
-// 返回 25 条结果的网页内容
+// 获取豆瓣小组的内容 start 表示从第几条开始，默认 0
+// 返回 25 条结果的网页内容，翻页时 start=25
 func GetGroup(name string, start ...int) (*goquery.Document, error) {
-	resp, err := httpClient.Get(getGroupURL(name, start...))
+	url := groupURL(name, start...)
+	log.Printf("\t\t[Info] try to fetch url: %s\n", url)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("new request err: %w", err)
+	}
+	req = requestMiddleware(req)
+
+	resp, err := httpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
+	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		if resp.StatusCode == http.StatusForbidden {
 			return nil, ErrorForbidden
 		}
-		return nil, ErrorTopicDelete
+		return nil, fmt.Errorf("response status %d err %w", resp.StatusCode, ErrorTopicDelete)
 	}
 
-	return goquery.NewDocumentFromResponse(resp)
+	return goquery.NewDocumentFromReader(resp.Body)
 }
